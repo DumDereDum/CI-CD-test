a97239c7ef8d4490e16edaf3c7337dae65d4be93
dumderedum <batters21@yandex.ru>
2020-04-17 12:42:42 +0300

    docker test 1.0

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 IMAGE="batters21/devops-18pi-dumderedum" 
-
+GIT_VERSION=$(git describe --always --abbrev --tags --long)
 docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
